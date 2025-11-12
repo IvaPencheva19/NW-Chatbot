c43f7cd0996813830cdf3f396e71ada8af32a0be
IvaPencheva19 <92177605+IvaPencheva19@users.noreply.github.com>
2025-11-12 10:22:22 +0200

    Init app. Add example json config file for chatbot. Add types.

---
+++ src/types/types.ts
@@ -0,0 +1,41 @@
+export type BlockType = 'write_message' | 'wait_for_response' | 'detect_intent';
+
+export interface ChatbotIntent {
+    name: string;
+    examples: string[];
+    next: string;
+}
+
+export interface BaseBlock {
+    id: string;
+    type: BlockType;
+    next?: string;
+}
+
+export interface WriteMessageBlock extends BaseBlock {
+    type: 'write_message';
+    message: string;
+}
+
+export interface WaitForResponseBlock extends BaseBlock {
+    type: 'wait_for_response';
+}
+
+export interface DetectIntentBlock extends BaseBlock {
+    type: 'detect_intent';
+    intents: ChatbotIntent[];
+    fallback_next: string;
+}
+
+export type ChatbotBlock =
+    | WriteMessageBlock
+    | WaitForResponseBlock
+    | DetectIntentBlock;
+
+export interface ChatbotConfig {
+    id: string;
+    name: string;
+    description?: string;
+    start_block: string;
+    blocks: Record<string, ChatbotBlock>;
+}
